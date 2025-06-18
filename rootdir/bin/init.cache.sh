dc0fb1e1d87c2b34efaf92434c3b44c0b75c6aee
Glautinor <glautinor@gmail.com>
2025-06-18 02:32:30 +0000

    appropriatly set cache script

--- rootdir/bin/init.cache.sh
+++ rootdir/bin/init.cache.sh
@@ -1,6 +1,17 @@
 #!/system/bin/sh
 
-setprop sys.lmk.minfree_levels 4096:0,5120:100,8192:200,24576:250,32768:900,47360:950
-device_config put activity_manager max_cached_processes 16
+while true; do
+  # Get the current hour (in 24-hour format)
+  current_hour=$(date +%H)
+
+  # Check if the current hour is 3 AM
+  if [ "$current_hour" -eq 3 ]; then
+    # Execute your script
+    sh /vendor/bin/init.cache.sh
+  fi
+
+  # Wait for 1 hour before checking the time again
+  sleep 3600
+done
 
 echo "Infinity X script last executed on $(date)" > /data/local/tmp/init.cache.txt

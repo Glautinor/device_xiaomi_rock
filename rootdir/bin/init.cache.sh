43b0dfe201df1875948b206f3809cc6c4e926690
Glautinor <glautinor@gmail.com>
2025-07-02 10:19:50 +0530

    set max backgroud apps from 8 to 16

--- rootdir/bin/init.cache.sh
+++ rootdir/bin/init.cache.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
 
-device_config put activity_manager max_cached_processes 8
+device_config put activity_manager max_cached_processes 16
 
 echo "Infinity X script last executed on $(date)" > /data/local/tmp/init.cache.txt
